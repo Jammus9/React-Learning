807ffe1629210fc5282195df6fa6d34153fda74a
zaidjamshaid@gmail.com <37055437+Jammus9@users.noreply.github.com>
2021-11-08 04:55:57 -0800

    First Components

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1> The Blog</h1>
+            <div className="link">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+
+            </div>
+
+        </div>
+     );
+}
+ 
+export default Navbar;
